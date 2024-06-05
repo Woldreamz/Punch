ff45092125ddb1fb44c11b419aecb9cc3d006a6f
Rainer Nsa <woldreamz@gmail.com>
2024-06-05 08:08:37 +0100

    City of God

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,7 +57,7 @@ const Footer = () => {
                 {/* Sidebar with max width */}
                 <img src={Logo} alt="Footer Logo" />
                 {/* Footer logo image */}
-                <p className="pt-6 text-indigo-50 text-sm font-['Switzer'] py-4">
+                <p className="pt-6 text-indigo-50 text-sm py-4">
                   {/* Description paragraph with padding top */}
                   We take complex hiring processes - and simplify them.
                   Connecting you to the world&apos;s highly qualified talent
@@ -70,13 +70,13 @@ const Footer = () => {
                   {/* Link buttons container with margin */}
                   <Link
                     to="/"
-                    className="grow shrink basis-0 self-stretch justify-center items-center inline-flex bg-gray-800 text-indigo-50 hover:bg-yellow-500 border-none  font-['Switzer'] rounded-2xl mr-3 px-9 py-4 text-sm font-medium leading-3"
+                    className="grow shrink basis-0 self-stretch justify-center items-center inline-flex bg-gray-800 text-indigo-50 hover:bg-yellow-500 border-none rounded-2xl mr-3 px-9 py-4 text-sm font-medium leading-3"
                   >
                     Hire now
                   </Link>
                   <Link
                     to="/"
-                    className="grow shrink basis-0 self-stretch justify-center items-center inline-flex bg-gray-800 text-indigo-50 hover:bg-yellow-500 border-none font-['Switzer'] rounded-2xl mr-3 px-9 py-4 text-sm font-medium leading-3"
+                    className="grow shrink basis-0 self-stretch justify-center items-center inline-flex bg-gray-800 text-indigo-50 hover:bg-yellow-500 border-none rounded-2xl mr-3 px-9 py-4 text-sm font-medium leading-3"
                   >
                     Apply now
                   </Link>
